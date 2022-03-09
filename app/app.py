0494bf3db9f830cd1e45d5ca728ae4b49f40c574
ssriram1978 <sriramsridharjob78@gmail.com>
2022-03-08 19:37:52 -0500

    Added unit tests. Optimized the code and improved latency.

--- app/app.py
+++ app/app.py
@@ -1,18 +1,30 @@
 import json
+import os
+import pathlib
+import pickle
 
 import pandas as pd
-from surprise import dump
 import numpy as np
+import sys
+import traceback
 
-smd = pd.read_csv('https://s3.amazonaws.com/movielens.data/smd.csv', header=0, parse_dates=[0], index_col=0,
+# contains 5 important columns - 'title', 'vote_count', 'vote_average', 'year', 'id', 'movieId'.
+smd = pd.read_csv(os.path.join(pathlib.Path(__file__).parent.absolute(), 'smd.csv'), header=0,
+                  index_col=0,
                   squeeze=True)
-indices = pd.read_csv('https://s3.amazonaws.com/movielens.data/indices.csv', header=0, parse_dates=[0], index_col=0,
-                      squeeze=True)
-id_map = pd.read_csv('https://s3.amazonaws.com/movielens.data/id_map.csv', header=0, parse_dates=[0], index_col=0,
-                     squeeze=True)
+
+# Pre computed cosine similarity Numpy arrays.
+cosine_sim = np.load(os.path.join(pathlib.Path(__file__).parent.absolute(), 'cosine_sim.npy'))
+
+
+def load(file_name):
+    dump_obj = pickle.load(open(file_name, 'rb'))
+    return dump_obj['algo']
+
+
 # https://surprise.readthedocs.io/en/stable/matrix_factorization.html#surprise.prediction_algorithms.matrix_factorization.SVD
-_, svd = dump.load('svd')
-cosine_sim = np.load('cosine_sim.npy')
+# It takes in the user id and the movie id.
+svd = load(os.path.join(pathlib.Path(__file__).parent.absolute(), 'svd'))
 
 
 def lambda_handler(event, context):
@@ -21,23 +33,50 @@ def lambda_handler(event, context):
         print(f"event={event}")
         title = event['multiValueQueryStringParameters']['Title'][0]
         userId = event['multiValueQueryStringParameters']['UserId'][0]
-        idx = indices[title]
         print(f'Received Userid={userId}, Title={title}')
-        indices_map = id_map.set_index('id')
+        found_row = smd[smd['title'] == title]
+        idx = 0
+        cosine_idx = 0
+        for key, value in found_row.iterrows():
+            idx = value['id']
+            cosine_idx = key
+            break
+        print("STAGE 0 complete - Found the corresponding found_row = {}, idx={}, cosine_idx={} for the title {}."
+              .format(found_row, idx, cosine_idx, title))
 
-        # STAGE 1: Filter out top 26 movies based on cosine similarity.
-        sim_scores = list(enumerate(cosine_sim[int(idx)]))
+        # STAGE 1: Filter out top 20 movies based on cosine similarity.
+        sim_scores = list(enumerate(cosine_sim[int(cosine_idx)]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:26]
-        movie_indices = [i[0] for i in sim_scores]
+        sim_scores = sim_scores[1:20]
+        print(f'sim_scores={sim_scores}')
 
-        # STAGE 2: Use the SVD model that is built on 9220 movies,
-        movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year', 'id']]
-        movies['est'] = movies['id'].apply(lambda x: svd.predict(userId, indices_map.loc[x]['movieId']).est)
-        movies = movies.sort_values('est', ascending=False)
+        movie_indices = [i[0] for i in sim_scores]
+        print(f"Stage 1 complete - Found movie indices = {movie_indices} for top 20 titles that share the same cosine "
+              f"similarity with the passed in movie index.")
+
+        # STAGE 2: Use the SVD model that is built on 9220 movies
+        # For each of the 20 top movies that share cosine similarity with the passed in movie title,
+        # Fetch the title, vote count, vote average, year and id.
+        movies = smd.loc[movie_indices]
+        print("Stage 2 completed. Successfully fetched the title, vote count, vote average, year and id ")
+        print(movies)
+        # STAGE 3: Apply SVD algorithm predict() API call to find the estimation for the top 20 movie titles.
+        # movies['id'].apply(lambda x: print(f'x={x}'))
+        movies['est'] = movies['movieId'].apply(lambda x: svd.predict(userId, x).est)
+
+        movies = movies.sort_values(['est', 'year'], ascending=False)
+        print('movies={}'.format(movies))
+        print("Stage 3 completed. Successfully applied SVD predict() on the list of 20 movie indices.")
+
+        # STAGE 4: Pick top 10 movies from this list and send this back to the customer.
         top_10_movies = movies.head(10)['title'].tolist()
-    except:
-        pass
+        print("Stage 4 completed. Successfully fetched top 10 movies from the list and returning this back to the "
+              "customer.")
+
+    except Exception:
+        # printing stack trace
+        traceback.print_exception(*sys.exc_info())
+        print(traceback.format_exc())
     finally:
         return {
             'statusCode': 200,
